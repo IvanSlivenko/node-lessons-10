ad32fc34fd399b90b2487eb2756009e903117eed
IvanSlivenko <ivan8822@ulr.net>
2023-07-06 22:22:12 +0300

    06-07-2023-22-22

--- project-backend/routes/auth-routs.test.js
+++ project-backend/routes/auth-routs.test.js
@@ -10,35 +10,38 @@ const User =require("../models/user")
 
 const { DB_HOST_TEST, PORT } = process.env;
 
+
 describe("test signup route", () => {
     let server = null;
-  deforeAll(async () => {
+  beforeAll(async () => {
     await mongoose.connect(DB_HOST_TEST);
-
-    //запускаємо server
-    server = app.listen(PORT);
-  });
+    server = app.listen(PORT); //запускаємо server 
+  })
 
   afterAll(async() => {
     await mongoose.connection.close();
         server.close();
   })
+
   test("test correct signup data", async () => {
+
     const signupData = {
       name: "Ivan",
       email: "ivan8822@ukr.net",
-      password:"",
+      password:"123456"
       
     }
     const { body, statusCode } = await request(app).post("/api/auth/signup").send(signupData);
-    expect(statusCode).toBe(201);
+
+    
     expect(body.name).toBe(signupData.name);
     expect(body.email).toBe(signupData.email);
-    expect(body.password).toBe(signupData.password);
+    expect(statusCode).toBe(201);
+    // expect(body.password).toBe(signupData.password);
 
     const user = await User.findOne({ email: signupData.email });
     expect(user.email).toBe(signupData.email);
-    expect(user.email).toBe(signupData.email);
+    expect(user.name).toBe(signupData.name);
 
     
 
